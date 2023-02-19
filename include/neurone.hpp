02432d6cd8b724f513002a5f77f933ddd74b2c62
PA-Senger <pa.lambrecht@gmail.com>
2023-02-19 19:14:02 +0100

    updated neurone
    removed the 2d array system for W and dW
    added getter setter and some methods
    still need to add the initialization of W with random number
    still need to test the class and add unitary tests

--- include/neurone.hpp
+++ include/neurone.hpp
@@ -9,23 +9,40 @@ class neurone {
         pfSigma = nullptr, pfDsigma = nullptr, m_W = nullptr, m_dW = nullptr;
     }
     // construtor taking size of entry vector 
-    neurone (int n) {
-    m_size = n;
-    m_po = 0;
-    // todo
-    }
+    neurone (int n);
     // copy constructor
     neurone (const neurone & ne);
     // destructor
     ~neurone();
+
+    // getter setter
+    int getSize () const;
+    double getW (int i) const;
+    double getDw (int i)const;
+    double getPo () const;
+    void setW (double w, int i);
+    void setDw (double dw, int i);
+    void setSigma (double (*pfS)(double d));
+    void setDsigma (double (*pfDs)(double d));
+
+    // operator
+    neurone operator= (const neurone & ne);
+
+    // methods
+    void WOnes ();
+    void WRandom (); // todo
+    void dWZeros ();
+    void evaluation (const double *X);
+    int getSize (const double * arr) const; // return the size of any 1D array
+
     
 
     private:
     int m_size;
-    double (*pfSigma)(double * tab);
-    double (*pfDsigma)(double * tab);
-    double **m_W;
-    double **m_dW;
+    double (*pfSigma)(double d);
+    double (*pfDsigma)(double d);
+    double *m_W;
+    double *m_dW;
     double m_po;
 
 
--- src/neurone.cpp
+++ src/neurone.cpp
@@ -1,27 +1,107 @@
 #include "../include/neurone.hpp"
+#include <iostream>
 
-
+neurone:: neurone (int n) {
+    m_size = n;
+    m_po = 0.;
+    pfSigma = nullptr;
+    pfDsigma = nullptr;
+    m_W = new double [n];
+    m_dW = new double [n];
+    for (int i=0; i<n; ++i) {
+        m_W[i] = 0.;
+        m_dW[i] = 0.;
+    }
+}
 neurone:: neurone (const neurone & ne) {
     m_size = ne.m_size;
     m_po = ne.m_po;
     pfSigma = ne.pfSigma; // pointer to the same "object"
     pfDsigma = ne.pfDsigma; // functions arent object so they cant be deep copied
-    m_W = new double * [2]; // 2D arrray
-    m_W[0] = new double[m_size]; // vecteur W
-    m_W[1] = new double[1]; // biais b
-    m_dW = new double * [2];
-    m_dW[0] = new double[m_size]; // vecteur dW
-    m_dW[1] = new double[1]; // db
-    for (int j=0; j<m_size; ++j) {
-        m_W[0][j] = ne.m_W[0][j]; // copy of W
-        m_dW[0][j] = ne.m_dW[0][j]; // copy of dW 
+    m_W = new double [m_size];
+    m_dW = new double [m_size];
+    for (int i=0; i<m_size; ++i) {
+        m_W[i] = ne.m_W[i];
+        m_dW[i] = ne.m_dW[i];
     }
-    m_W[1] = ne.m_W[1]; // copy of b
-    m_dW[1] = ne.m_dW[1]; // copy of db
-}
+} 
 neurone:: ~neurone () { 
-    for (int i=0; i<2; i++) {
-        delete [] m_W[i];
-        delete [] m_dW[i];
+    delete m_W;
+    delete m_dW;    
+}
+int neurone:: getSize () const { return m_size; }
+double neurone:: getW (int i) const { return m_W[i]; }
+double neurone:: getDw (int i) const { return m_dW[i] ; }
+double neurone:: getPo () const { return m_po; }
+void neurone:: setW (double w, int i) { m_W[i] = w; }
+void neurone:: setDw (double dw, int i) { m_dW[i] = dw; }
+void neurone:: setSigma (double (*pfS)(double d)) { pfSigma = pfS; }
+void neurone:: setDsigma (double (*pfDs)(double d)) { pfDsigma = pfDs; }
+neurone neurone:: operator= (const neurone & ne) {
+    if ( this != &ne ) {
+        m_size = ne.m_size;
+        m_po = ne.m_po;
+        pfSigma = ne.pfSigma;
+        pfDsigma = ne.pfDsigma;
+        if (m_W != nullptr) {
+            delete m_W;
+            double * m_W;
+        }
+        if (m_dW != nullptr) {
+            delete m_dW;
+            double * m_W;
+        }
+        m_W = new double [m_size];
+        m_dW = new double [m_size];
+        for (int i=0; i<m_size; ++i) {
+            m_W[i] = ne.m_W[i];
+            m_dW[i] = ne.m_dW[i];
         }
     }
+    return *this;
+}
+void neurone:: WOnes () {
+    if ( m_W != nullptr ) {
+       delete [] m_W;
+       double * m_W;
+    }
+    m_W = new double [m_size];   
+    for (int i=0; i<m_size; ++i) {
+        m_W[i] = 1.;
+    }
+}
+void neurone:: dWZeros () {
+    if ( m_dW != nullptr ) {
+       delete [] m_dW;
+       double * m_dW;
+    }
+    m_dW = new double [m_size];   
+    for (int i=0; i<m_size; ++i) {
+        m_dW[i] = 0.;
+    }
+}
+void neurone:: evaluation (const double *X) {
+    double dot = 0;
+    if ( m_size != getSize(X) ) {
+        std::cout << "Error : W and X are not the same dimensions ! \n";
+        exit(1);
+    }
+    for (int i=0; i<m_size; ++i) {
+        dot += m_W[i] * X[i];
+    }
+    m_po = pfSigma(dot);
+}
+int neurone:: getSize (const double * arr) const {
+    int size = sizeof(arr)/sizeof(arr[0]);
+    return size;
+}
+
+
+
+
+
+
+
+
+
+
